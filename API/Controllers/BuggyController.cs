b544092e263c2203b57fe78a7f38765cbea4952f
moudAB <81109037+moudAB@users.noreply.github.com>
2022-01-09 22:54:58 -0500

    End of section 5

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Errors;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BuggyController:BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BuggyController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("notfound")]
+        public ActionResult GetNotFoundrequest()
+        {
+            var thing= _context.Products.Find(42);
+            if(thing==null)
+            {
+                return NotFound(new ApiResponse(404));
+            }
+            return Ok();
+        }
+        [HttpGet("ServerError")]
+        public ActionResult GetServerError()
+        {
+            var thing= _context.Products.Find(42);
+            var thingReturn = thing.ToString();
+            return Ok();
+        }
+
+        [HttpGet("BadRequest/{id}")]
+        public ActionResult GetBadRequest(int id)
+        {
+            return Ok(new ApiResponse(200));
+        }
+        [HttpGet("BadRequest")]
+        public ActionResult GetNotFoundRequest()
+        {
+            return BadRequest(new ApiResponse(400));
+        }
+    }
+}
